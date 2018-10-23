fafeb2f6a7a49ca98026c146ec166842871e7f43
root <root@localhost.localdomain>
2018-10-23 11:15:05 +0700

    Edit AJAX

--- ssh/urls.py
+++ ssh/urls.py
@@ -5,5 +5,6 @@ app_name = "ssh"
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^(?P<id>[0-9]+)/$', views.detail, name='detail'),
+	url(r'^(?P<id>[0-9]+)/connect$', views.connect, name='connectSSH'),
     # ex: /polls/5/results/
 ]
--- ssh/views.py
+++ ssh/views.py
@@ -3,17 +3,49 @@ from .models import SSHPermission ,SSH
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
 import paramiko
+from django.http import JsonResponse
+
 
 def index(request):
-	if request.user.is_authenticated():
+	if not request.user.is_superuser:
 		try:
 			obj = get_object_or_404(SSHPermission, user__username=request.user.username)
 			context = {'obj':obj,}
-		except SSHPermission.DoesNotExist:
+		except :
 			context = None	
 		return render(request, 'index.html', context)			
 	else:
 		return render(request,'index.html')	
 
 def detail(request, id):
-	return render(request,'detail.html')	
+	if not request.user.is_superuser:
+		try:
+			getSSH = SSH.objects.filter(sshpermission__user__username='test',sshpermission__permission=True,id=id)
+			obj = get_object_or_404(getSSH, id=id)
+			context = {'obj':obj}
+		except SSHPermission.DoesNotExist:
+			context = None	
+		return render(request, 'detail.html', context)			
+	else:
+		return render(request,'detail.html')	
+
+def connect(request, id):
+	try:
+		obj = get_object_or_404(SSH,pk=id)
+		ssh = paramiko.SSHClient()
+		ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		ssh.connect(obj.ip, obj.port, username=obj.username,password=obj.password)
+		data = {
+			'status':'Connect successfully',
+			'code':1
+		}
+	except:
+		data = {
+			'status':"Can't connect",
+			'code':0
+		}
+	return JsonResponse(data)		
+
+def disconnect(request, id):
+	if ssh:
+		ssh.close()
